f34fdad1d359fb737af3076ae9115932a5a6ff3b
junior <juniorjerameel1519@gmail.com>
2019-02-22 12:15:51 -0400

    Diseno 70% completado

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,9 +6,16 @@ module.exports = (app)=>{
     app.get('/', (req, res)=>{
         res.render('index');
     })
-
+    app.get('/Equipos', (req, res)=>{
+        res.render('posiciones');
+    })
+    app.get('/Noticias', (req, res)=>{
+        res.render('noticias');
+    })
     app.use('/players', require('./players'))
     app.use('/account', require('./account'))
     app.use('/admin', require('./admin'))
     
+    
 }
+
